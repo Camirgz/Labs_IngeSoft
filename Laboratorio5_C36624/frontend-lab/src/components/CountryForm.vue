9aaad29bcac2760d77fc2b091ee8f40cfd73418a
Camila <camila.rodriguez.aguila@gmail.com>
2026-04-12 21:48:54 -0600

    Router

---
+++ Laboratorio5_C36624/frontend-lab/src/components/CountryForm.vue
@@ -0,0 +1,16 @@
+﻿<template>
+    <div>
+        <h1>Formulario para crear un país</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CountryForm',
+        setup() {
+            return {}
+        },
+    };
+</script>
+
+<style></style>
